3038e4593b36b56ff534b9b6ddb407404e70b13a
Uriel Ortiz <22001668@galileo.edu>
2025-05-23 10:37:43 -0600

    style: update button styling in ContactForm for improved aesthetics

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -77,7 +77,7 @@ const ContactForm = () => {
         <div className="mt-10">
           <button
             type="submit"
-            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
+            className="block w-full rounded-lg px-3.5 py-2.5 text-center text-sm font-medium text-white shadow-xs bg-cyber hover:shadow-lg hover:shadow-cyan-500/30 transition-all"
           >
             Enviar Mensaje
           </button>
